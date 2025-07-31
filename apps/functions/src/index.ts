c162084a5bdbc33a59537df41e7894523bda63fd
theridwanade <theridwanade@gmail.com>
2025-07-31 16:05:56 +0100

    Retrived newly created user data and event context on firebase cloud functions

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -1,19 +1,18 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import * as functionsV1 from 'firebase-functions/v1';
+export const CreateNewUser = functionsV1.auth.user().onCreate(async (user, context) => {
+  try {
+    const { uid, email, displayName, photoURL } = user;
+    // Log the user creation event
+    console.log('New user created:', {
+      uid,
+      email,
+      displayName,
+      photoURL,
+      timestamp: context.timestamp,
+    });
 
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+  } catch (error) {
+    console.error('Error creating new user:', error);
+  }
+})
