d765977d45576e9add30385f6dfe78fb32e3d21f
theridwanade <theridwanade@gmail.com>
2025-07-31 17:26:12 +0100

    Implement page routing upon successful login

--- apps/web/src/components/GoogleLogin.tsx
+++ apps/web/src/components/GoogleLogin.tsx
@@ -3,14 +3,18 @@ import {Button} from "@/components/ui/button";
 import {FcGoogle} from "react-icons/fc";
 import {GoogleAuthProvider, signInWithPopup} from "firebase/auth";
 import {auth} from "@/lib/firebase.config";
+import {useRouter} from "next/navigation";
 
 const GoogleLogin = () => {
+    const router = useRouter();
     const provider = new GoogleAuthProvider();
     const handleGoogleLogin = async () => {
         try {
             const result = await signInWithPopup(auth, provider);
             const user = result.user;
-            console.log("Google Login Successful", user);
+            if(user) {
+                router.push("/landing");
+            }
         } catch (error) {
             console.error("Google Login Error", error);
         }
