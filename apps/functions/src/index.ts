f1470ea653994af32730ec98443df7deeef29db4
theridwanade <theridwanade@gmail.com>
2025-07-31 17:19:50 +0100

    User data creation on firestore implemented via cloud functions

--- apps/functions/src/index.ts
+++ apps/functions/src/index.ts
@@ -1,17 +1,24 @@
 import * as functionsV1 from 'firebase-functions/v1';
+import * as admin from 'firebase-admin';
+admin.initializeApp();
+
+
 export const CreateNewUser = functionsV1.auth.user().onCreate(async (user, context) => {
   try {
     const { uid, email, displayName, photoURL } = user;
-    // Log the user creation event
-    console.log('New user created:', {
-      uid,
+
+    const result = await admin.firestore().collection("users").doc(uid).set({
       email,
       displayName,
       photoURL,
-      timestamp: context.timestamp,
-    });
+      createdAt: context.timestamp,
+      updatedAt: context.timestamp,
+    })
 
-
+    if(!result){
+        throw new Error('Failed to create user document in Firestore');
+    }
+    console.log('New user created:', uid, email, displayName);
   } catch (error) {
     console.error('Error creating new user:', error);
   }
